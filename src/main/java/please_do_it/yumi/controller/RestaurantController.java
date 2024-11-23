47f12807b31968a2860f7cb8130ecb618c723132
ceo92 <king9243@naver.com>
2024-11-24 00:47:10 +0900

    feat: 체크박스용 태그 컬렉션 데이터 뷰로 넘기는 데이터 컨트롤러에 추가

--- src/main/java/please_do_it/yumi/controller/RestaurantController.java
+++ src/main/java/please_do_it/yumi/controller/RestaurantController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import please_do_it.yumi.constant.ContainFoodType;
+import please_do_it.yumi.constant.MoodType;
+import please_do_it.yumi.constant.ProvideServiceType;
+import please_do_it.yumi.constant.RestaurantType;
 import please_do_it.yumi.domain.Restaurant;
 import please_do_it.yumi.dto.RestaurantSearchCond;
 import please_do_it.yumi.service.RestaurantService;
@@ -20,9 +24,30 @@ public class RestaurantController {
 
   private final RestaurantService restaurantService;
 
+  @ModelAttribute("containFoodTypes")
+  public ContainFoodType[] containFoodTypes(){
+    return ContainFoodType.values();
+  }
+
+  @ModelAttribute("provideServiceTypes")
+  public ProvideServiceType[] provideServiceTypes(){
+    return ProvideServiceType.values();
+  }
+
+  @ModelAttribute("restaurantTypes")
+  public RestaurantType[] restaurantTypes(){
+    return RestaurantType.values();
+  }
+
+  @ModelAttribute("moodTypes")
+  public MoodType[] moodTypes(){
+    return MoodType.values();
+  }
+
+
 
   @GetMapping("/restaurants")
-  public String getRestaurants(@ModelAttribute RestaurantSearchCond restaurantSearchCond, Model model){
+  public String getRestaurants(@ModelAttribute("restaurantSearchCond") RestaurantSearchCond restaurantSearchCond, Model model){
     List<Restaurant> restaurants = restaurantService.findRestaurants(restaurantSearchCond);
     model.addAttribute("restaurants", restaurants);
     return "restaurant/restaurants";

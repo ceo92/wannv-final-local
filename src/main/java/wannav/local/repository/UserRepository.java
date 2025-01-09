9dbe78d0c5b08d424b063eeb390d3cc1f12ec53a
ceo92 <king9243@naver.com>
2025-01-09 13:58:07 +0900

    refactor: userrepository 삭제

--- src/main/java/wannav/local/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package wannav.local.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import wannav.local.domain.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
